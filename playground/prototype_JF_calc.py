55d6f0012288f51a809e636c71866f7f1bc5241e
Mike Ireland <michael.ireland@anu.edu.au>
2021-09-11 18:52:29 +1000

    Verified approximate luminosity conservation. Minor changes.

--- playground/prototype_JF_calc.py
+++ playground/prototype_JF_calc.py
@@ -11,13 +11,32 @@ tau, r, theta ,?, ?, log(rho), ?, ?, v(km/s)
 
 
 theta is 5040/T, which is multiplied by the excitation energy in eV to get the Boltzmann factor
+
+The M10 was the "P Series" and the M12 the "M Series"
+e.g. M12L3P10 started with a 4910 L_sun model:
+0.120E+010.491E+040.260E+03
+... which matches the start of OA07.M10DM
+0.120E+010.491E+040.260E+03
+
+Checking PW versus IA05... thets is similar around tau=1, but 
+PW is much too warm in outer layers. I have to start with OA instead.
+
+plt.plot(pwtab['r'], pwtab['theta'])
+
+S10 = planck_mult_const * nus**3/(np.exp(planck_exp_const*pwtab['theta'][10]*nus)-1)
+
+T = 5040/pwtab['theta']
+
+S10_1 = c.h/c.c**2 * (nus*u.Hz)**3/(np.exp(c.h/T[10]/u.K*nus*u.Hz/c.k_B)-1)
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-import struct
+import time
 import pdb
 import astropy.io.fits as pyfits
+import astropy.constants as c
+import astropy.units as u
 from scipy.interpolate import RectBivariateSpline
 plt.ion()
 
@@ -81,7 +100,7 @@ def read_opm(fname):
         line = f.readline()
         nth = int(line[:3])
         nprl = int(line[3:6])
-        tit = line[7:-1]
+        title = line[7:-1]
         line = f.readline()
         ss = line.split()
         th = float(ss[0]) + np.arange(nth)*float(ss[1])
@@ -109,7 +128,7 @@ def read_opm(fname):
             dwaves[i] = line[1]
             absn[i] = read_one_tab(f, nth, nprl)
             scat[i] = read_one_tab(f, nth, nprl)
-    return th, prl, kross, mu, pel, waves, nus, dnus, absn, scat
+    return th, prl, kross, mu, pel, waves, nus, dnus, absn, scat, title
               
 def compute_geometry(rs, central_nmu=5):
     """
@@ -131,8 +150,8 @@ def compute_geometry(rs, central_nmu=5):
         
     #Now compute x and mu
     x = np.zeros_like(mus)
-    for i in range(len(rs) + central_nmu):
-        for j in range(np.maximum(i-central_nmu,0),len(rs)):
+    for j in range(0,len(rs)):
+        for i in range(0,central_nmu+j+1):
             x[i,j] = np.sqrt(rs[j]**2 - p[i]**2)
             mus[i,j] = x[i,j]/rs[j]
 
@@ -157,7 +176,7 @@ def compute_geometry(rs, central_nmu=5):
     delta_s = x[:-1,1:] - x[:-1,:-1]
     return delta_s, Jw, Hw, mus
     
-def compute_grid(delta_s, k_r, source_fns, central_nmu=5):
+def compute_grid(delta_s, k_r):
     """
     For x single wavelength, compute the grid of \Delta I coefficients
     going in the positive and negative directions.
@@ -166,6 +185,7 @@ def compute_grid(delta_s, k_r, source_fns, central_nmu=5):
     #this was the integration variable chosen in rtransfer_notes. Usually 
     #called t, which is also confusing!
     #The following arrays can be empty, but we want to debug...
+    central_nmu = delta_s.shape[0]-delta_s.shape[1]
     y = np.zeros((delta_s.shape[0], delta_s.shape[1]))
     S_C1 = np.zeros_like(y)
     S_C2 = np.zeros_like(y)
@@ -182,15 +202,24 @@ def compute_grid(delta_s, k_r, source_fns, central_nmu=5):
     
     return S_C1, S_C2, expdy
 
-def compute_rtransfer(S_C1, S_C2, expdy, Jw, Hw, central_nmu=5):
+def compute_rtransfer(S_C1, S_C2, expdy, Jw, Hw):
     """
-    Compute the radiative transfer matrices for J and H
+    Compute the radiative transfer matrices for J and H. There are 5 input
+    matrices:
+    
+    Parameters
+    ----------
+    S_C1:
+        (np-1, nr-1) array. 1st coefficients of the source function
+    S_C2:
+        
     """
     #Firstly, compute the optical depths, which we call y here just because 
     #this was the integration variable chosen in rtransfer_notes. Usually 
     #called t, which is also confusing!
     #The following arrays can be empty, but we want to debug...
     nr = Jw.shape[1]
+    central_nmu = Jw.shape[0]-Jw.shape[1]
     Jmat = np.zeros((nr, nr))
     Hmat = np.zeros((nr, nr))
     Iinner_C = np.zeros((central_nmu+nr, nr))
@@ -219,7 +248,7 @@ def compute_rtransfer(S_C1, S_C2, expdy, Jw, Hw, central_nmu=5):
         #i is the y value. We have already covered the mu=0 ray, so don't
         #need to consider it again.
         for i in range(0,central_nmu+j):
-            #Integrating outwards along the incoming ray.
+            #Integrating inwards along the outgoing ray.
             exptau = 1
             for k in range(0,j):
                 Jmat[j,j-k] += exptau*S_C1[i,j-k-1]*Jw[i,j]
@@ -241,7 +270,8 @@ def compute_rtransfer(S_C1, S_C2, expdy, Jw, Hw, central_nmu=5):
                     Hmat[j,k] += exptau*Iinner_C[i,k]*Hw[i,j]
         
     return Jmat, Hmat
-                       
+
+                      
 if __name__=="__main__":
     fits_fname = 'OPM00_os4300_0212.fits'
     #Read in the dynamical model
@@ -259,10 +289,12 @@ if __name__=="__main__":
         th = ff[0].header['CRPIX1'] + ff[0].header['CDELT1']*np.arange(absn.shape[2])
         prl = ff[0].header['CRPIX2'] + ff[0].header['CDELT2']*np.arange(absn.shape[1])
         nus = ff[4].data['nu']
+        waves = ff[4].data['wave']
+        dnus = ff[4].data['dnus']
     else:
         #Read in the opacity file
         opmfile = '/Users/mireland/code/scholz_code/tables/OPM00_os4300_0212'
-        th, prl, kross, mu, pel, waves, nus, dnus, absn, scat = read_opm(opmfile)
+        th, prl, kross, mu, pel, waves, nus, dnus, absn, scat, title = read_opm(opmfile)
         hdu1 = pyfits.PrimaryHDU(absn)
         hdu1.header['CRPIX1'] = (th[0], 'Theta in 1/eV (5040/T)')
         hdu1.header['CDELT1'] = (th[1]-th[0], 'Delta theta in 1/eV')
@@ -280,20 +312,43 @@ if __name__=="__main__":
             
     #waves[1500] is 1.7 microns and a good wavelength to try radiative transfer.
     #Now we find opacity k from rho and kappa.
-    w_ix = 1500
-    absn_func = RectBivariateSpline(prl, th, absn[w_ix])
-    #Now interpolate for our layers
-    kappa_r = absn_func(pwtab['prl'],pwtab['theta'],grid=False)
-    k_r = 10**(kappa_r + pwtab['lrho'])
-    #Compute the initial source function fot these layers
-    planck_exp_const = 6.626e-34/1.602e-19
-    planck_mult_const = 2*6.626e-34/3e8**2
-    source_fns = planck_mult_const * nus[w_ix]**3/(np.exp(planck_exp_const*pwtab['theta']*nus[w_ix])-1)
-
-    #We have everything we need! Lets compute J and H, in three steps.
-    S_C1, S_C2, expdy = compute_grid(delta_s, k_r, source_fns)
-    Jmat, Hmat = compute_rtransfer(S_C1, S_C2, expdy, Jw, Hw)
+    w_ixs = np.arange(len(nus)) #[1500]#np.range(100) #np.range(len(nus))
+    nr = len(pwtab['r'])
+    Js = np.empty((len(w_ixs), nr))
+    Hs = np.empty((len(w_ixs), nr))
+    for ix, w_ix in enumerate(w_ixs): 
+        start = time.time()
+        absn_func = RectBivariateSpline(prl, th, absn[w_ix])
+        
+        #Now interpolate for our layers
+        kappa_r = absn_func(pwtab['prl'],pwtab['theta'],grid=False)
+        k_r = 10**(kappa_r + pwtab['lrho'])
+        
+        #Compute the initial source function fot these layers
+        planck_exp_const = 6.626e-34/1.38e-23/5040
+        planck_mult_const = 2*6.626e-34/3e8**2
+        source_fns = planck_mult_const * nus[w_ix]**3/(np.exp(planck_exp_const*pwtab['theta']*nus[w_ix])-1)
     
+        #We have everything we need! Lets compute J and H, in three steps.
+        S_C1, S_C2, expdy = compute_grid(delta_s, k_r)
+        Jmat, Hmat = compute_rtransfer(S_C1, S_C2, expdy, Jw, Hw)
+        Js[ix] = np.dot(Jmat, source_fns)
+        Hs[ix] = np.dot(Hmat, source_fns)
+        #print("Total Time: ", time.time()-start)
+        if ((ix % 100)==0):
+            print(w_ix)
+
+    #Compute the total flux
+    Ft = np.zeros((nr))
+    for i in range(nr):
+        Ft[i] = 4*np.pi*np.sum(Hs[:,i]*dnus)
+    plt.clf()
+    plt.plot(((Ft*u.W/u.m**2)*4*np.pi*(pwtab['r']*u.cm)**2).to(u.L_sun).value)
+    plt.xlabel('Layer')
+    plt.ylabel('Luminosity (L_sun)')
+    flux = Ft[10]*u.W/u.m**2
+    print( "Rough Teff: ", ((flux/c.sigma_sb)**(1/4)).si )
     #To Check against a reference calculation...
     #np.save('Jmat.npy', Jmat)
+    #np.save('Hmat.npy', Jmat)
     
